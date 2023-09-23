33f0068fd037e682cc274351f85dd8673b973331
Mulia Abror Khairul <muliausman12@gmail.com>
2023-09-23 15:22:33 +0700

    Initial commit

---
+++ UnitTestProject1/RepositoryManagerTests.cs
@@ -0,0 +1,152 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using YourProject;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class RepositoryManagerTests
+    {
+        [TestMethod]
+        public void Initialize_ShouldCreateEmptyRepository()
+        {
+            // Arrange & Act
+            RepositoryManager manager = new RepositoryManager();
+
+            // Assert
+            Assert.IsNotNull(manager); // Ensure manager is created
+        }
+
+        [TestMethod]
+        public void Register_ValidJsonForType1_ShouldSucceed()
+        {
+            // Arrange
+            RepositoryManager manager = new RepositoryManager();
+            string validJson = "{\"name\": \"John Doe\", \"age\": 30}";
+
+            // Act
+            manager.Register("item1", validJson, 1);
+
+            // Assert
+            string retrievedContent = manager.Retrieve("item1");
+            Assert.AreEqual(validJson, retrievedContent);
+        }
+
+        [TestMethod]
+        public void Register_ValidXmlForType2_ShouldSucceed()
+        {
+            // Arrange
+            RepositoryManager manager = new RepositoryManager();
+            string validXml = "<person><name>John Doe</name><age>30</age></person>";
+
+            // Act
+            manager.Register("item1", validXml, 2);
+
+            // Assert
+            string retrievedContent = manager.Retrieve("item1");
+            Assert.AreEqual(validXml, retrievedContent);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void Register_InvalidJsonForType1_ShouldThrowException()
+        {
+            // Arrange
+            RepositoryManager manager = new RepositoryManager();
+            string invalidJson = "invalid json";
+
+            // Act
+            manager.Register("item1", invalidJson, 1);
+
+            // Assert
+            // The ExpectedException attribute ensures that an exception is thrown
+        }
+
+        [TestMethod]
+        public void Retrieve_ExistingItem_ShouldReturnContent()
+        {
+            // Arrange
+            RepositoryManager manager = new RepositoryManager();
+            string content = "{\"name\": \"Bambang\", \"age\": 10}";
+            manager.Register("item1", content, 1);
+
+            // Act
+            string retrievedContent = manager.Retrieve("item1");
+
+            // Assert
+            Assert.AreEqual(content, retrievedContent);
+
+        }
+
+        [TestMethod]
+        public void Retrieve_NonExistentItem_ShouldReturnNull()
+        {
+            // Arrange
+            RepositoryManager manager = new RepositoryManager();
+
+            // Act
+            string retrievedContent = manager.Retrieve("nonexistentItem");
+
+            // Assert
+            Assert.IsNull(retrievedContent);
+        }
+
+        [TestMethod]
+        public void GetType_ExistingItem_ShouldReturnItemType()
+        {
+            // Arrange
+            RepositoryManager manager = new RepositoryManager();
+            string content = "{\"name\": \"Bambang\", \"age\": 10}";
+            manager.Register("item1", content, 1);
+
+            // Act
+            int itemType = manager.GetType("item1");
+
+            // Assert
+            Assert.AreEqual(1, itemType);
+        }
+
+        [TestMethod]
+        public void GetType_NonExistentItem_ShouldReturnNegativeOne()
+        {
+            // Arrange
+            RepositoryManager manager = new RepositoryManager();
+
+            // Act
+            int itemType = manager.GetType("nonexistentItem");
+
+            // Assert
+            Assert.AreEqual(-1, itemType);
+        }
+
+        [TestMethod]
+        public void Deregister_ExistingItem_ShouldRemoveItem()
+        {
+            // Arrange
+            RepositoryManager manager = new RepositoryManager();
+            string content = "{\"name\": \"Bambang\", \"age\": 10}";
+            manager.Register("item1", content, 1);
+
+            // Act
+            manager.Deregister("item1");
+
+            // Assert
+            Assert.IsNull(manager.Retrieve("item1"));
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void Deregister_NonExistentItem_ShouldThrowException()
+        {
+            // Arrange
+            RepositoryManager manager = new RepositoryManager();
+
+            // Act
+            manager.Deregister("nonexistentItem");
+
+            // Assert
+            // The ExpectedException attribute ensures that an ArgumentException is thrown
+        }
+
+    }
+}
---
+++ YourProject/RepositoryManager.cs
@@ -0,0 +1,109 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace YourProject
+{
+    public class RepositoryManager
+    {
+        private Dictionary<string, Tuple<string, int>> repository;
+
+        public RepositoryManager()
+        {
+            Initialize();
+        }
+
+        public void Initialize()
+        {
+            repository = new Dictionary<string, Tuple<string, int>>();
+        }
+
+        public void Register(string itemName, string itemContent, int itemType)
+        {
+            // Perform validation against itemContent based on itemType
+            // If validation fails, throw an exception
+            if (itemType == 1 && !IsValidJson(itemContent))
+            {
+                throw new ArgumentException("Invalid JSON content for item type 1.");
+            }
+
+            // Example: Check if itemContent is valid XML for itemType 2
+            if (itemType == 2 && !IsValidXml(itemContent))
+            {
+                throw new ArgumentException("Invalid XML content for item type 2.");
+            }
+
+            if (repository.ContainsKey(itemName))
+            {
+                throw new ArgumentException("An item with the same name already exists.");
+            }
+
+            repository[itemName] = Tuple.Create(itemContent, itemType);
+        }
+
+        private bool IsValidJson(string json)
+        {
+            try
+            {
+                JObject.Parse(json);
+                return true;
+            }
+            catch (JsonException)
+            {
+                return false;
+            }
+        }
+
+        private bool IsValidXml(string xml)
+        {
+            try
+            {
+                XDocument.Parse(xml);
+                return true;
+            }
+            catch (XmlException)
+            {
+                return false;
+            }
+        }
+
+        public string Retrieve(string itemName)
+        {
+            if (repository.ContainsKey(itemName))
+            {
+                return repository[itemName].Item1;
+            }
+
+            return null; // or throw an exception if item not found
+        }
+
+        public int GetType(string itemName)
+        {
+            if (repository.ContainsKey(itemName))
+            {
+                return repository[itemName].Item2;
+            }
+
+            return -1; // or throw an exception if item not found
+        }
+
+        public void Deregister(string itemName)
+        {
+            if (repository.ContainsKey(itemName))
+            {
+                repository.Remove(itemName);
+            }
+            else
+            {
+                throw new ArgumentException("Item not found.");
+            }
+        }
+    }
+
+}
